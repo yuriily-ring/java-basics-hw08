8a97d330a0f96f21c9db1ace02f5bbe98b46679a
kermek <kek277@gmail.com>
2020-04-25 18:16:54 +0300

    adding test data

--- src/RegExp.java
+++ src/RegExp.java
@@ -3,13 +3,14 @@
  */
 public class RegExp {
 
-  /**
-   * Get IP address from text.
-   * @param text text to parse
-   * @return String with IP address
-   */
-  public static String getIpAddress(String text) {
-    // TODO fill in code here for extracting IP address using Regular Expression
-    return "";
-  }
+    /**
+     * Get IP address from text.
+     *
+     * @param text text to parse
+     * @return String with IP address
+     */
+    public static String getIpAddress(String text) {
+        // TODO fill in code here for extracting IP address using Regular Expression
+        return "";
+    }
 }
--- src/RegExpTest.java
+++ src/RegExpTest.java
@@ -1,31 +1,35 @@
-import static org.testng.Assert.assertEquals;
-
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
 public class RegExpTest {
 
-  /**
-   * Test data.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "text")
-  public static Object[][] text() {
-    return new Object[][]{
-        {"8.8.8.8", "8.8.8.8"},
-        {"http://192.168.1.1/index.html", "192.168.1.1"},
-        {"This text contains a valid IP address 254.254.254.254, find it!", "254.254.254.254"},
-        {"IP could not have negative numbers like -1.-2.-3.-4", ""},
-        {" Invalid IP: 300.300.300.300!", ""}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "text")
+    public static Object[][] text() {
+        return new Object[][]{
+                {"8.8.8.8", "8.8.8.8"},
+                {"http://192.168.1.1/index.html", "192.168.1.1"},
+                {"This text contains a valid IP address 254.254.254.254, find it!", "254.254.254.254"},
+                {"IP could not have negative numbers like -1.-2.-3.-4", ""},
+                {" Invalid IP: 300.300.300.300!", ""},
+                {" Invalid IP: 256.256.256.256!", ""},
+                {"invalid:256.256.256.256", ""},
+                {"valid IP: 255.255.255.255!", "255.255.255.255"},
+                {"invalid:255.255.255.255", "255.255.255.255"}
+                // TODO add 2 more test data here
+        };
+    }
 
-  @Test(dataProvider = "text")
-  public void testSumArray(String text, String expectedResult) {
-    String actualResult = RegExp.getIpAddress(text);
+    @Test(dataProvider = "text")
+    public void testSumArray(String text, String expectedResult) {
+        String actualResult = RegExp.getIpAddress(text);
 
-    assertEquals(actualResult, expectedResult, "IP address is not correct");
-  }
+        assertEquals(actualResult, expectedResult, "IP address is not correct");
+    }
 }
