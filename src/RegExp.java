7a5a851be3b69184199861885ab9c61882e8b39f
Yuriy Lytovchenko <yuri.litovchenko@gmail.com>
2021-01-12 17:42:26 +0200

    Homework 8

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,3 +1,6 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Regular expressions.
  */
@@ -11,6 +14,12 @@ public class RegExp {
      */
     public static String getIpAddress(String text) {
         // TODO fill in code here for extracting IP address using Regular Expression
+        String oneSegment = "([0-9]{1,2}|1[0-9]{1,2}|2[0-4][0-9]|25[0-5])";
+        String regexString = "\\b" + oneSegment + "\\." + oneSegment + "\\." + oneSegment + "\\." + oneSegment + "\\b";
+        Pattern pattern = Pattern.compile(regexString);
+        Matcher matcher = pattern.matcher(text);
+        if (matcher.find())
+            return matcher.group(0);
         return "";
     }
 }
